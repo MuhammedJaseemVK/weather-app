2057dee98e8b9330941bf987db59cad639f2b941
Jaseem <muhammedjaseemvk96@gmail.com>
2023-11-08 10:22:51 +0530

    added enter to search feature

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
     setCity(e.target.value);
   }
 
+  const handleKeyDown = (e)=>{
+    if(e.key==='Enter'){
+      setTrigger(trigger+1)
+    }
+  }
+
   useEffect(()=>{
     setCity('London');
     setTrigger(trigger+1)
@@ -20,7 +26,7 @@ function App() {
       <div className='flex flex-col items-center gap-2 p-3 bg-blue-500 rounded-md'>
         <p className='font-bold'>Weather App</p>
         <div className='flex gap-2'>
-          <input type='text' className='rounded-xl px-2' placeholder='Enter a city' value={city} onChange={handleOnChange} />
+          <input type='text' className='rounded-xl px-2' placeholder='Enter a city' onKeyDown={handleKeyDown} value={city} onChange={handleOnChange} />
           <img className='rounded-xl bg-white p-1' src='assets/search.png' alt='search' onClick={()=>{setTrigger(trigger+1)}} />
         </div>
         <Api city={city} trigger={trigger} setCity={setCity} />
