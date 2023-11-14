637f55fc2650207116e718e929280d9ea8e807c8
Jaseem <muhammedjaseemvk96@gmail.com>
2023-11-14 21:15:02 +0530

    added icon and made responsive

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Api from './components/api/api';
+import { CiSearch } from "react-icons/ci";
 
 function App() {
   const [city, setCity] = useState('');
-  const [trigger,setTrigger] =useState(0);
+  const [trigger, setTrigger] = useState(0);
 
   const handleOnChange = (e) => {
     setCity(e.target.value);
   }
 
-  const handleKeyDown = (e)=>{
-    if(e.key==='Enter'){
-      setTrigger(trigger+1)
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setTrigger(trigger + 1)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     setCity('London');
-    setTrigger(trigger+1)
-  },[])
+    setTrigger(trigger + 1)
+  }, [])
 
   return (
     <div className='flex justify-center items-center h-screen bg-slate-900'>
-      <div className='flex flex-col items-center gap-2 p-3 bg-gradient-to-b from-violet-600 to-indigo-600 rounded-md w-80 h-80'>
+      <div className='flex mx-5 sm:mx-0 flex-col items-center gap-2 p-3 bg-gradient-to-b from-violet-600 to-indigo-600 rounded-md w-fit h-72 sm:h-80'>
         <p className='font-bold text-white text-xl'>Weather App</p>
         <div className='flex gap-2 w-full'>
           <input type='text' className='rounded-xl px-2 w-full' placeholder='Enter a city' name='city' onKeyDown={handleKeyDown} value={city} onChange={handleOnChange} />
-          <img className='rounded-full bg-white p-1' src='assets/search.png' alt='search' onClick={()=>{setTrigger(trigger+1)}} />
+          <CiSearch className='rounded-full bg-white p-1 text-4xl' onClick={() => { setTrigger(trigger + 1) }} />
         </div>
         <Api city={city} trigger={trigger} setCity={setCity} />
       </div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Weather(props) {
   return (
-    <div className='flex flex-col gap-1 items-center w-[90%] text-white'>
+    <div className='flex flex-col gap-1 items-center w-[90%] text-white '>
       <img src={`https://openweathermap.org/img/wn/${props.weather.icon}@2x.png`} className='w-auto' alt="" />
-      <h1 className='font-bold text-3xl'>{Number(props.weather.temp).toFixed(1)} &deg;c</h1>
-      <h1 className='font-bold text-xl'>{props.weather.cityName}</h1>
-      <div className='w-full p-3 flex justify-between'>
-        <div className='flex gap-2 items-center text-base'>
+      <h1 className='font-bold text-xl'>{Number(props.weather.temp).toFixed(1)} &deg;c</h1>
+      <h1 className='font-bold text-lg'>{props.weather.cityName}</h1>
+      <div className='w-full flex justify-between'>
+        <div className='flex gap-2 items-center text-xs sm:text-base'>
           <img className='h-[20px]' src="assets/humidity.png" alt="" />
           <div className="flex flex-col">
             <p>{props.weather.humidity} %</p>
             <p>Humidity</p>
           </div>
         </div>
-        <div className='flex gap-2 items-center text-base'>
+        <div className='flex gap-2 items-center text-xs sm:text-base'>
           <img className='h-[20px]' src="assets/wind.png" alt="" />
           <div className="flex flex-col">
             <p>{Number((props.weather.windSpeed)*(18/5)).toFixed(1)} Km/hr</p>
